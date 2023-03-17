d2f0a03b57b74c83d32142c056f6498630e78934
zhig1ov <zhiglov.artem@yandex.ru>
2023-03-17 15:46:28 +0500

    feat: add help information through the commander

---
+++ bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+const program = new Command();
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.');
+   
+program.parse(process.argv);
