1db1fd505dea51ab19f0c31f8077d307d56bed1b
zhig1ov <zhiglov.artem@yandex.ru>
2023-03-17 16:00:20 +0500

    feat: add arguments and option to commander

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,9 @@ const program = new Command();
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .argument('filepath1')
+  .argument('filepath2')
+  .option('-f, --format <type>', 'output format');
    
 program.parse(process.argv);
